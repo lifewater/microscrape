c22718fd87e77fe254df4da31bbe1be992de4a23
Joe ONeill <jfo@vitol.com>
2025-05-14 07:58:04 -0500

    removed quotes from sku label

--- src/main.py
+++ src/main.py
@@ -149,7 +149,7 @@ def update_metrics():
 def prometheus_metrics():
     lines = []
     for sku, data in GPUs.items():
-        labels = f'brand={data["brand"]},type={data["type"]},model={data["model"]},ram={data["ram"]},sku="{sku}"'
+        labels = f'brand={data["brand"]},type={data["type"]},model={data["model"]},ram={data["ram"]},sku={sku}'
         lines.append(f'gpu_stock{{{labels}}} {data["stock"]}')
         lines.append(f'gpu_price{{{labels}}} {data["price"]}')
     return "\n".join(lines) + "\n"
